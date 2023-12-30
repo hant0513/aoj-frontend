a8a84b2eb7748f444fb1a22eaab14aca43944556
hant0513 <3216185388@qq.com>
2023-12-30 13:34:20 +0800

    通用布局文件

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header"><GlobalHeader /></a-layout-header>
+      <a-layout-content class="content"><router-view /></a-layout-content>
+      <a-layout-footer class="footer">@By hant</a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basicLayout {
+}
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+}
+#basicLayout .content {
+  background: linear-gradient(to right, #313c4e, #fff);
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #24bb9b;
+  padding: 16px;
+  position: absolute; /* 绝对定位：固定至底部 */
+  bottom: 0;
+  /* 撑满整个底部区域 */
+  left: 0;
+  right: 0;
+
+  text-align: center; /* 文字居中 */
+}
+</style>
+
+<script>
+import GlobalHeader from "@/components/GlobalHeader.vue";
+export default {
+  components: { GlobalHeader },
+};
+</script>
